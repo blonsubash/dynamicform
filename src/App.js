a24f7f82a8b17e76574462d518e1aee93b5afb74
Subash Lama <subashlama@Subashs-MacBook-Pro.local>
2023-04-26 11:56:49 +0545

    dyanmic ui rendered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { inputFormElements } from "./formElements";
 
 function App() {
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      firstName: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required(),
+      lastName: Yup.string().required(),
+    }),
+    onSubmit: (values) => {
+      console.log("values");
+    },
+  });
+  console.log("valuessss", formik.values);
+  console.log("errors", formik.errors);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid style={{ padding: "80px 5px 0 5px" }}>
+        <Card style={{ maxWidth: 600, margin: "0 auto" }}>
+          <CardContent>
+            <Typography>Dynamic Form</Typography>
+            <form onSubmit={formik.handleSubmit}>
+              <Grid container spacing={1}>
+                {inputFormElements.map((input) => (
+                  <Grid spacing={1}>
+                    <TextField {...input} onChange={formik.handleChange} />
+                    <div></div>
+                  </Grid>
+                ))}
+              </Grid>
+            </form>
+            <Grid>
+              <Button type="submit" variant="outlined">
+                Submit
+              </Button>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
     </div>
   );
 }
